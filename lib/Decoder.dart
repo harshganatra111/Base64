a8fe3fbded4da469af33ce20bf12871f777b6bfb
harshganatra111 <harshganatra111@gmail.com>
2020-03-22 23:41:17 +0530

    navigation to encoder and decoder added

---
+++ lib/Decoder.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Decoder extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Decoder"),
+        backgroundColor: Color.fromRGBO(0 , 0, 55, 1.0),
+      ),
+      body: Center(
+        
+      ),
+    );
+  }
+}
